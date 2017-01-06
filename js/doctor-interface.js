35dc0f46d0e5a2797798ef5d9877aa8b53e02861
Jim Padilla <jp_code2013@yahoo.com>
2017-01-06 15:20:56 -0800

    Add bootstrap classes for styling image media

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,10 +1,9 @@
 var DoctorData = require('./../js/doctor.js').doctorModule;
 var displayDoctors = function(medicalIssue, doctorEntry) {
-  //medicalIssue param not needed unless printing it out
-  $('.showCondition').append("<p class='center-text'>" + "You inputted: " + medicalIssue.capitalizeFirst() + "</p>");
+  $('.showCondition').append("<h2>" + "You inputted: " + medicalIssue.capitalizeFirst() + "</h2>");
   doctorEntry.forEach(function(entry) {
-    $('.showDoctors').append("<li class='center-text'>" + "<div class='media'>" + "<div class='media-left media-middle'>" + "<img class='media-object' src='" + entry.profile.image_url + "'>" + "</div>" + "<div class='media-body'>" + "<p>" + entry.profile.first_name + ' ' + entry.profile.last_name + ' ' + entry.profile.title + "</p>" + "<p>" +"Specialty: " + entry.specialties[0].name + "</p>" + "</li>" + "</div>" + "</div>"
-                                  );
+    $('.showDoctors').append("<li class='center-text'>" + "<div class='media'>" + "<div class='media-left media-middle'>" + "<img class='media-object' src='" + entry.profile.image_url + "'>" + "</div>" + "<div class='media-body media-middle'>" + "<p>" + entry.profile.first_name + ' ' + entry.profile.last_name + ' ' + entry.profile.title + "</p>" + "<p>" +"Specialty: " + entry.specialties[0].name + "</p>" + "</li>" + "</div>" + "</div>" + "<hr>"
+    );
   });
 };
 
