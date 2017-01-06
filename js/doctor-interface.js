151b4c3545094aabcace6675888d624a96e9c1ed
Jim Padilla <jp_code2013@yahoo.com>
2017-01-06 11:27:14 -0800

    Get data from API

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,5 +1,14 @@
-var Doctor = require('./../js/doctor.js').doctorModule;
+var DoctorData = require('./../js/doctor.js').doctorModule;
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
-  
+
+  var newDoctorData = new DoctorData();
+  $('#search-form').submit(function(event) {
+    event.preventDefault();
+    var medicalIssue = $('#condition-input').val();
+
+    newDoctorData.getDoctors(medicalIssue, apiKey)
+  });
+
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,5 +1,21 @@
-function Doctor() {
-
+function DoctorData() {
+  this.retrieved = [];
 }
 
-exports.doctorModule = Doctor;
+DoctorData.prototype.getDoctors = function(medicalIssue, apiKey) {
+  var docDataObj = [];
+
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+     .then(function(result) {
+        result.data.forEach(function(dataEntry) {
+          docDataObj.push(dataEntry);
+        });
+      })
+     .fail(function(error){
+        console.log("fail");
+      });
+
+  this.retrieved = docDataObj;
+};
+
+exports.doctorModule = DoctorData;
