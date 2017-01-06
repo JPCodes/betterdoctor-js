172f853ade44feca4841077cd046976a8bdedbb3
Jim Padilla <jp_code2013@yahoo.com>
2017-01-06 15:14:06 -0800

    Modify styling for physician images

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,12 +3,7 @@ var displayDoctors = function(medicalIssue, doctorEntry) {
   //medicalIssue param not needed unless printing it out
   $('.showCondition').append("<p class='center-text'>" + "You inputted: " + medicalIssue.capitalizeFirst() + "</p>");
   doctorEntry.forEach(function(entry) {
-    $('.showDoctors').append("<li class='center-text'>" + entry.profile.first_name + ' '
-                                    + entry.profile.last_name + ' '
-                                    + entry.profile.title
-                                    + "<ul>"
-                                    + "<li class='center-text'>" + "Specialty: " + entry.specialties[0].name + "</li>" + "</li>"
-                                    + "</ul>"
+    $('.showDoctors').append("<li class='center-text'>" + "<div class='media'>" + "<div class='media-left media-middle'>" + "<img class='media-object' src='" + entry.profile.image_url + "'>" + "</div>" + "<div class='media-body'>" + "<p>" + entry.profile.first_name + ' ' + entry.profile.last_name + ' ' + entry.profile.title + "</p>" + "<p>" +"Specialty: " + entry.specialties[0].name + "</p>" + "</li>" + "</div>" + "</div>"
                                   );
   });
 };
@@ -19,6 +14,7 @@ $(document).ready(function(){
   $('#search-form').submit(function(event) {
     event.preventDefault();
     newDoctorData.retrieved = [];
+    $('.showCondition').empty();
     $('.showDoctors').empty();
     var medicalIssue = $('#condition-input').val();
     newDoctorData.getDoctors(medicalIssue, displayDoctors);
