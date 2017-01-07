6ee875c2b18c4378f4928439fe46976098737963
Jim Padilla <jp_code2013@yahoo.com>
2017-01-06 16:47:43 -0800

    remove commented js

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,4 @@
 var DoctorData = require('./../js/doctor.js').doctorModule;
-// var displayDoctors = function(medicalIssue, doctorEntry) {
-//   $('.showCondition').append("<h2>" + "You inputted: " + medicalIssue.capitalizeFirst() + "</h2>");
-//   doctorEntry.forEach(function(entry) {
-//     $('.showDoctors').append("<li class='center-text'>" + "<div class='media'>" + "<div class='media-left media-middle'>" + "<img class='media-object' src='" + entry.profile.image_url + "'>" + "</div>" + "<div class='media-body media-middle'>" + "<p>" + entry.profile.first_name + ' ' + entry.profile.last_name + ' ' + entry.profile.title + "</p>" + "<p>" +"Specialty: " + entry.specialties[0].name + "</p>" + "</li>" + "</div>" + "</div>" + "<hr>"
-//     );
-//   });
-// };
 var displayDoctors = function(medicalIssue, doctorEntry) {
   $('.showCondition').append("<h2>" + "You inputted: " + medicalIssue.capitalizeFirst() + "</h2>");
   doctorEntry.forEach(function(entry) {
